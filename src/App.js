c8a0cda2357c2e6b4ebf3cc40702b26f565fa463
Artiom <artemilico1337@gmail.com>
2024-05-09 07:43:10 +0300

    setting for yml

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
+import React from 'react'
+import { useState } from 'react'
 import logo from './logo.svg';
-import './App.css';
+import Post from './Post'
+import { useQuery } from 'react-query';
+import client from './react-query-client'
+
+
+const fetcher = url => fetch(url).then(res => res.json());
 
 function App() {
+
+  const [postID, setPostID] = useState(null)
+  const { isLoading, data: posts } = useQuery('posts', () => fetcher('https://jsonplaceholder.typicode.com/posts'), {
+    select: posts => posts.slice(0, 5)
+  })
+
+  if (isLoading) return <h1>Loading...</h1>
+
+  if (postID !== null) {
+    return <Post postID={postID} goBack={() => setPostID(null)} />
+  }
+
+  function mutateTitle(id){
+    client.setQueryData(['post', id], oldData => {
+      if(oldData) {
+        return {
+          ...oldData,
+          title: 'boom boom mutated'
+        }
+      }
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {posts.map(post => {
+
+        const cachedPost = client.getQueryData(['post', post.id]);
+
+        return <p key={post.id}>
+          <b>{cachedPost ? '(visited)' : ''}</b><a onClick={() => setPostID(post.id)} href="#">{post.id} - {post.title}</a>
+          <button onClick={() => mutateTitle(post.id)}>mutate The Title</button>
+          </p>
+      })}
     </div>
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './global.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools'
+import client from './react-query-client'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <QueryClientProvider client={client}>
+        
+        <App />
+        <ReactQueryDevtools />
+    </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
